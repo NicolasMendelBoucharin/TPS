dc66be257b05c27c6fc2ab2486bc2603d7e83931
NicolasMendelBoucharin <nicolasmendel34@gmail.com>
2024-09-29 17:21:59 +0200

    Fin du TP2 ACF

--- ACF/TP2/TP2.thy
+++ ACF/TP2/TP2.thy
@@ -44,8 +44,88 @@ nitpick
 
 (*Exercice 5:*)
 
-fun delete:: 'a list \<Rightarrow> 'a \<Rightarrow> 'a list
+fun delete::" 'a  \<Rightarrow> 'a list \<Rightarrow> 'a list"
   where
-"delete(x#xs) = (if (List.member xs x)then  )"
+"delete x []  = []" |
+"delete x (y#ys)  = (if x=y then delete x ys else y#delete x ys)"
 
+
+(*Exercice 6:*)
+
+lemma  "
+\<not>(List.member(delete x List)(x))
+"
+  nitpick
+  sorry
+
+(*Exercice 7:*)
+
+fun intersection:: "'a list \<Rightarrow> 'a list \<Rightarrow> 'a list"
+  where
+"intersection xs [] = []" |
+"intersection [] xs = []" |
+"intersection (x#xs) ys = (if List.member ys x then (x#intersection xs ys) else intersection xs ys)"
+(*Exercice 8:*)
+
+lemma "
+List.member (intersection L K) x = (List.member L x) \<and> (List.member K x) 
+"
+  nitpick
+  oops
+
+(*Exercice 9:*)
+
+lemma "
+isSet ( intersection K L)
+"
+  nitpick
+  oops
+
+
+(*Exercice 10:*)
+
+fun union :: "'a list \<Rightarrow> 'a list \<Rightarrow> 'a list"
+  where 
+"union xs [] = xs" |
+"union [] xs = xs" |
+"union (x#xs) ys = (if List.member ys x then (x#union xs ys) else union xs ys)"
+
+value " union [1::nat, 2, 3] [2::nat, 5, 4]"
+
+(*visiblement faux du coup mdr*)
+
+
+(*Exercice 11:*)
+
+lemma  "
+List.member (union L K) x =((List.member L x) \<or> (List.member K x))
+"
+  nitpick
+  oops
+
+(*Exercice 12:*)
+
+lemma "
+(isSet(union L K)) = (isSet(K) \<or> isSet(L))
+"
+  nitpick
+  oops
+
+
+(*Exercice 13:*)
+
+fun equal:: "'a list \<Rightarrow> 'a list \<Rightarrow> bool"
+  where 
+"equal [] [] = True" |
+"equal xs [] = False" |
+"equal [] ys = False" |
+"equal (x#xs) ys =( if List.member ys x then ( equal xs (delete x ys)) else False)"
+
+value "equal [1::nat, 2 , 3] [2::nat, 1, 3, 4] "
+
+(*Exercice 14:*)
+lemma "
+ 
+"
 end
+
