0fb6a197b60586c6f20275a192b45b7327734209
NicolasMendelBoucharin <nicolasmendel34@gmail.com>
2024-10-05 14:31:56 +0200

    Advancement in ACF

--- ACF/TP2/TP2.thy
+++ ACF/TP2/TP2.thy
@@ -24,23 +24,36 @@ value "clean[1::nat, 2, 3, 4, 3, 2, 1]"
 
 (*Exercice 3:*)
 
-lemma exercice3:"
-List.member(clean(x)) = List.member(x)
+lemma memberclean:"
+(List.member (clean xs) y ) \<longleftrightarrow> (List.member xs y)
 "
-  nitpick
+  apply (induct xs)
+   apply simp
+  by (metis clean.simps(2) member_rec(1))
+
+
+
+
+ 
 
 (*Exercice 4:*)
 
-lemma exercice4:"
-\<forall>L. isSet(L) \<longrightarrow> clean(L)=L
+lemma issetclean:"
+(isSet L) \<longrightarrow> ((clean L) = L)
 "
-nipick
-  oops
 
-lemma exercice4bis:"
-isSet(clean l )
+  apply (induct L)
+  using clean.simps(1) apply blast
+  by simp
+
+
+lemma issetcleanbis:"
+isSet(clean L)
 "
-nitpick
+
+  apply (induct L)
+  apply simp
+  by (simp add: memberclean)
 
 (*Exercice 5:*)
 
@@ -49,14 +62,19 @@ fun delete::" 'a  \<Rightarrow> 'a list \<Rightarrow> 'a list"
 "delete x []  = []" |
 "delete x (y#ys)  = (if x=y then delete x ys else y#delete x ys)"
 
+value " delete 1 [1::nat, 2, 3, 1, 5]"
 
 (*Exercice 6:*)
 
-lemma  "
-\<not>(List.member(delete x List)(x))
+lemma memberdelete: "
+\<not>(List.member (delete x L)  x )
 "
-  nitpick
-  sorry
+  apply (induct L)
+  apply (simp add: member_rec(2))
+  by (metis delete.simps(2) member_rec(1))
+   
+
+  
 
 (*Exercice 7:*)
 
@@ -65,67 +83,99 @@ fun intersection:: "'a list \<Rightarrow> 'a list \<Rightarrow> 'a list"
 "intersection xs [] = []" |
 "intersection [] xs = []" |
 "intersection (x#xs) ys = (if List.member ys x then (x#intersection xs ys) else intersection xs ys)"
+
 (*Exercice 8:*)
 
-lemma "
-List.member (intersection L K) x = (List.member L x) \<and> (List.member K x) 
+lemma memberintersect: "
+List.member (intersection L K) x =( (List.member L x) \<and> (List.member K x) )
 "
-  nitpick
-  oops
-
+  apply (induct L)
+  apply (smt (verit, ccfv_SIG) intersection.elims member_rec(2) neq_Nil_conv)
+  by (smt (verit) intersection.elims list.discI list.sel(3) member_rec(1))
+ 
 (*Exercice 9:*)
 
-lemma "
-isSet ( intersection K L)
-"
-  nitpick
-  oops
 
+lemma setintersect: "
+isSet(L) \<and> isSet(K) \<longrightarrow> isSet(intersection L K)
+"
+  apply (induct L )
+  apply (metis intersection.simps(1) intersection.simps(2) isSet.elims(2))
+  by (smt (verit) intersection.elims isSet.simps(2) list.sel(3) memberintersect)
+
+  
+  
 
 (*Exercice 10:*)
 
-fun union :: "'a list \<Rightarrow> 'a list \<Rightarrow> 'a list"
+fun union :: "'a list  \<Rightarrow> 'a list \<Rightarrow> 'a list"
   where 
-"union xs [] = xs" |
 "union [] xs = xs" |
-"union (x#xs) ys = (if List.member ys x then (x#union xs ys) else union xs ys)"
+"union (x#xs) ys = union xs (x#(delete x ys))"
 
 value " union [1::nat, 2, 3] [2::nat, 5, 4]"
 
-(*visiblement faux du coup mdr*)
+
 
 
 (*Exercice 11:*)
+lemma memberunionhelp: "
+((List.member L x )) \<longrightarrow> (List.member (union L K) x)
 
-lemma  "
+"
+
+  apply (induct L)
+  using member_rec(2) apply force
+  
+
+
+lemma memberunion: "
 List.member (union L K) x =((List.member L x) \<or> (List.member K x))
 "
-  nitpick
-  oops
+
+  
+  
+
 
 (*Exercice 12:*)
 
 lemma "
-(isSet(union L K)) = (isSet(K) \<or> isSet(L))
+ (isSet(K) \<and> isSet(L))\<longrightarrow>(isSet(union L K)) 
 "
   nitpick
-  oops
+  
 
 
 (*Exercice 13:*)
 
+(*
 fun equal:: "'a list \<Rightarrow> 'a list \<Rightarrow> bool"
   where 
 "equal [] [] = True" |
 "equal xs [] = False" |
 "equal [] ys = False" |
 "equal (x#xs) ys =( if List.member ys x then ( equal xs (delete x ys)) else False)"
+*)
+
+fun includee:: " 'a list \<Rightarrow> 'a list \<Rightarrow> bool"
+  where
+"includee []  xs = True"|
+"includee (x#xs) [] = False"|
+"includee (x#xs) ys = ((List.member ys x) \<and> (includee xs ys)) "
+
+
+fun equal:: "'a list \<Rightarrow> 'a list \<Rightarrow> bool"
+  where
+  "equal xs ys = ((includee xs ys) \<and> (includee ys xs))"
+  
 
 value "equal [1::nat, 2 , 3] [2::nat, 1, 3, 4] "
 
 (*Exercice 14:*)
 lemma "
- 
+ (equal xs ys) \<longleftrightarrow> ((List.member ys x) \<longleftrightarrow> (List.member xs x))
 "
+
+  nitpick
 end
 
